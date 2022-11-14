18a912cea7bdb1399707cde345f86e5131f65a94
Tmeng <tonymengt@gmail.com>
2022-11-14 13:50:06 -0500

    added Nav component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@
 import { useState } from 'react';
 import './App.css';
 import About from './componets/About';
+import Nav from './componets/Nav';
 
 function App() {
-  const [contactSelected, setcontactSelected] = useState(false)
-  const [selectedTab, seSelectedTab] = useState()
+  const [tabs] = useState([
+    {name: 'About'},
+    {name: 'Portfolio'},
+    {name: 'Contact'},
+    {name: 'Resume'},
+  ])
+  const [selectedTab, setSelectedTab] = useState(tabs[0])
 
 
   return (
     <div className="App">
       <main>
         <About></About>
+        <Nav 
+          tabs = {tabs}
+          selectedTab = {selectedTab}
+          setSelectedTab = {setSelectedTab}
+        ></Nav>
       </main>
     </div>
   );
--- src/componets/Nav.js
+++ src/componets/Nav.js
@@ -1,5 +1,22 @@
 import React from "react";
 
-const Nav = () => {
-    
+const Nav = (props) => {
+    const { tabs, selectedTab, setSelectedTab} = props
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    {tabs.map((tab) => (
+                        <li className={`${selectedTab.name === tab.name && 'active'}`} key={tab.name}>
+                            <span onClick={() => {setSelectedTab(tab)}}>{tab.name}</span>
+                        </li>
+                    ))}
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    )
 }
+
+export default Nav;
